64ca43bca4be24ec2c7d1aba147c9029dd4a4b91
januarpancaran <januar352@gmail.com>
2026-03-31 21:49:41 +0700

    feat: adding guard check for cloudflare gpg and apt source

--- scripts/setup-cloudflare-warp.sh
+++ scripts/setup-cloudflare-warp.sh
@@ -3,10 +3,13 @@
 source utils.sh
 
 install_warp() {
-	curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | "$SUDO_CMD" gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
-	echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ noble main" | "$SUDO_CMD" tee /etc/apt/sources.list.d/cloudflare-client.list
-	"$SUDO_CMD" apt update
-	install_cmd cloudflare-warp
+    if [ ! -f /etc/apt/sources.list.d/cloudflare-client.list ]; then
+        curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | "$SUDO_CMD" gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ noble main" | "$SUDO_CMD" tee /etc/apt/sources.list.d/cloudflare-client.list
+    fi
+
+    "$SUDO_CMD" apt update
+    install_cmd cloudflare-warp
 }
 
 install_warp
