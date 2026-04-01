78fc76589d511e23c3923ac5052b9e7ee6a555cb
januarpancaran <januar352@gmail.com>
2026-04-01 11:05:43 +0700

    feat: adding Bibata Cursor

---
+++ scripts/setup-cursor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source utils.sh
+
+install_bibata() {
+	local CURSOR_DIR="$HOME/.icons"
+	mkdir -p "$CURSOR_DIR"
+
+	local latest_ver=$(curl -s https://api.github.com/repos/ful1e5/Bibata_Cursor/releases/latest | jq -r .tag_name | sed 's/v//')
+
+	local tar_file="Bibata-Modern-Classic.tar.xz"
+
+	if ! curl -LO https://github.com/ful1e5/Bibata_Cursor/releases/download/v${latest_ver}/${tar_file}; then
+		echo "Failed to fetch $tar_file"
+		return 1
+	fi
+
+	if ! tar -xf "$tar_file" -C "$CURSOR_DIR"; then
+		echo "Failed to extract $tar_file"
+		return 1
+	fi
+
+	rm -f "$tar_file"
+}
+
+install_bibata
