5812f5a15857c32c50bd26d4a31e95e80cdb0d97
Januar Pancaran <januar352@gmail.com>
2026-05-28 20:14:51 +0700

    feat!: removing spotify install

--- scripts/setup-entertainment.sh
+++ scripts/setup-entertainment.sh
@@ -11,24 +11,7 @@ install_obs() {
   install_cmd obs-studio
 }
 
-install_spotify() {
-  if ! cmd_exists spotify; then
-    if [ ! -f /etc/apt/sources.list.d/spotify.list ]; then
-      if ! curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc | "$SUDO_CMD" gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg; then
-        echo "Failed to fetch spotify key"
-        return 1
-      fi
-
-      echo "deb https://repository.spotify.com stable non-free" | "$SUDO_CMD" tee /etc/apt/sources.list.d/spotify.list
-
-      update_cmd
-      install_cmd spotify-client
-    fi
-  fi
-}
-
 install_entertainment() {
   install_mpv
   install_obs
-  install_spotify
 }
